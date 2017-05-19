21904188a84cfc06a01aff7ad160317bfec1bac3
Robin Robin <rrobin@monetate.com>
2017-05-19 10:23:10 -0400

    remove unsupported python versions from setup.py

--- setup.py
+++ setup.py
@@ -46,10 +46,6 @@ setup(
         'Intended Audience :: Developers',
         'Natural Language :: English',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements
